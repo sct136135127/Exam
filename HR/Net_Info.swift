a23ad64540b9022d8e55c885a201da21ece59582
sunchutao <1050405528@qq.com>
2019-12-23 13:02:57 +0800

    1.0

---
+++ HR/Net_Info.swift
@@ -0,0 +1,13 @@
+//
+//  Net_Info.swift
+//  HR
+//
+//  Created by JASON on 2019/12/23.
+//  Copyright © 2019 JASON. All rights reserved.
+//
+
+import UIKit
+
+class Net_Info: NSObject {
+
+}
---
+++ HR/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  HR
+//
+//  Created by JASON on 2019/12/23.
+//  Copyright © 2019 JASON. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
